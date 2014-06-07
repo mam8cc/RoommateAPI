fe1bcbd133456a247a92ff2289af1eeedbbee9f4
Mike McClannahan <mam8cc@gmail.com>
2014-06-07 12:58:46 -0500

    initial commit

---
+++ src/main/java/com/roommateAPI/config/JerseyApplication.java
@@ -0,0 +1,10 @@
+package com.roommateAPI.config;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class JerseyApplication extends ResourceConfig {
+    public JerseyApplication() {
+        register(JacksonFeature.class);
+    }
+}
